37d6dabaec53fa51317c99adf35c9a62e9bb282c
SkyRzn <alexandr.sky@gmail.com>
2020-10-12 12:42:14 +0300

    Clean test/config.c.

--- test/config.c
+++ test/config.c
@@ -7,14 +7,11 @@ CHEAT_TEST(config,
 	const char *module_names[] = {"container1:module1", "container2:module2"};
 	int i;
 
-	dbg("!!!!!!!!!!! 0\n");
 	iterator = first_config_iterator(NULL);
 	cheat_assert(iterator == NULL);
-	dbg("!!!!!!!!!!! 1\n");
 
 	iterator = first_config_iterator("/nonexistent_config_file_path");
 	cheat_assert(iterator == NULL);
-	dbg("!!!!!!!!!!! 2\n");
 
 	i = 0;
 	config_foreach("./test/config/config1.cfg", iterator) {
@@ -22,7 +19,6 @@ CHEAT_TEST(config,
 		cheat_assert(iterator->name != NULL);
 		cheat_assert(strcmp(iterator->name, instance_names[i]) == 0);
 		cheat_assert(iterator->module_name != NULL);
-		dbg("'%s'=='%s'\n", iterator->module_name, module_names[i]);
 		cheat_assert(strcmp(iterator->module_name, module_names[i]) == 0);
 
 		i++;
