f94c1a8cb6b32d18168b5548654fd2a78a8c0fbe
SkyRzn <alexandr.sky@gmail.com>
2020-10-14 11:32:45 +0300

    Add configuration (with COVERAGE=ON) and compilation to test.sh script.

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+cd build &&
+make clean &&
+cmake .. -DCOVERAGE=ON &&
+make &&
+cd .. &&
 build/test/sdr_server_test
-lcov --capture --directory . --output-file build/test/coverage.info
+lcov --capture --directory . --output-file build/test/coverage.info &&
 genhtml build/test/coverage.info --output-directory build/test/html
