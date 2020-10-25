47888ff022eb07b495bc7876a9c144ed7f40e9d2
SkyRzn <alexandr.sky@gmail.com>
2020-10-25 12:33:04 +0300

    Change void init_factory() return to int.

--- src/lib/factory.c
+++ src/lib/factory.c
@@ -6,28 +6,27 @@
 #include <stdio.h> // TEST
 
 
-void init_factory(const char *config_path)
+int init_factory(const char *config_path)
 {
 	config_iterator_t *iterator;
 	instance_t *instance, *prev_instance = NULL;
 	config_t config;
 
-	dbg_assert_not_null(config_path, );
+	dbg_assert_not_null(config_path, -EINVAL);
 
-	if (load_config(&config, config_path) != 0)
-		return;
+	dbg_assert_not_error_int(load_config(&config, config_path));
 
 	init_storage();
 
 	config_foreach(&config, iterator) {
-		dbg_assert_not_null(iterator->name, );
+		dbg_assert_not_null(iterator->name, -EINVAL);
 
 		instance = get_instance(iterator->name);
 
-		dbg_assert_not_null(instance, );
+		dbg_assert_not_null(instance, -ENOMEM);
 
 		if (iterator->module_name)
-			dbg_assert_not_error(set_instance_module(instance, iterator->module_name), );
+			dbg_assert_not_error_int(set_instance_module(instance, iterator->module_name));
 
 		if (prev_instance) {
 			instance->input = prev_instance;
@@ -47,4 +46,6 @@ void init_factory(const char *config_path)
 
 	free_storage();
 	free_config(&config);
+
+	return 0;
 }
